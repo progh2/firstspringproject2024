41244333f5e0724880a214fcc30374d61182f37b
Gihun Ham <ham@e-mirim.hs.kr>
2024-01-11 02:04:16 +0900

    프로젝트 최초 임포트

---
+++ src/main/java/kr/hs/emirim/ham/firstproject/FirstprojectApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.ham.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+
+}
